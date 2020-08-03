fc2357f2e51e8c1c0aa6181316b64523448297cd
Zhong, Yanghong <nju_yaho@apache.org>
2020-08-04 00:01:15 +0800

    KYLIN-4658 Add IT

---
+++ kylin-it/src/test/resources/query/sql_unionall/query04.sql
@@ -0,0 +1,33 @@
+-- unionall subquery under join
+select LSTG_FORMAT_NAME,
+       SLR_SEGMENT_CD,
+       CAL_DT,
+       sum(CNT) as CNT
+from
+  (select LSTG_FORMAT_NAME,
+          SLR_SEGMENT_CD,
+          CAL_DT,
+          sum(ITEM_COUNT) CNT
+   from TEST_KYLIN_FACT
+   where LSTG_FORMAT_NAME = 'ABIN'
+   group by LSTG_FORMAT_NAME,
+            SLR_SEGMENT_CD,
+            CAL_DT
+   UNION ALL select 'NON-ABIN' as LSTG_FORMAT_NAME,
+                    SLR_SEGMENT_CD,
+                    CAL_DT,
+                    case
+                        when SLR_SEGMENT_CD > 1000 then CNT * 2
+                        else CNT * 3
+                    end as CNT
+   from
+     (select SLR_SEGMENT_CD,
+             CAL_DT,
+             sum(ITEM_COUNT) CNT
+      from TEST_KYLIN_FACT
+      where LSTG_FORMAT_NAME <> 'ABIN'
+      group by SLR_SEGMENT_CD,CAL_DT))
+group by LSTG_FORMAT_NAME,
+         SLR_SEGMENT_CD,
+         CAL_DT
+order by CNT
